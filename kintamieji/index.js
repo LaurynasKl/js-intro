c5d846769537e28c00df7f4f15b5d4343eab32fa
Laurynas <klumbyslaurynas@gmail.com>
2023-09-19 11:28:28 +0300

    kintamieji

---
+++ kintamieji/index.js
@@ -0,0 +1,9 @@
+/*
+KINTAMIEJI 
+
+- numbers
+- strings (tekstas)
+- boolean (true/false)
+- array (kolekcija / sarasas / listas)
+- object (objektas - kompleksiniai informacijai aprasyti)
+*/
---
+++ kintamieji/number.js
@@ -0,0 +1,20 @@
+/*
+NUMBERS
+
+- teigiami / neigiami
+- sveikieji / desimtainiai
+- narmlus / nenormalus
+
+Iniciavimo budas:
+- const (default)
+- let (jei kinta informacija)
+- var (niekada)
+*/
+
+const amzius = 99;
+let pinigine = 0;
+var minusas = -7.56;
+
+console.log(amzius);
+console.log(pinigine);
+console.log(minusas);
