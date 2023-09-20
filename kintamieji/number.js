d212087cb46bc9a7ebe107db384b17eb5e7d8f8d
Laurynas <klumbyslaurynas@gmail.com>
2023-09-20 10:33:32 +0300

    kintamieji ir nodemone

--- kintamieji/number.js
+++ kintamieji/number.js
@@ -24,3 +24,35 @@ const x2 = 2;
 
 console.log(x1);
 console.log(x2);
+
+
+const n1 = 7;
+const n2 = 5;
+
+const sum = n1 + n2;
+console.log(sum);
+
+const diff = n1 - n2;
+console.log(diff);
+
+const multi = n1 * n2;
+console.log(multi);
+
+const div = n1 / n2;
+console.log(div);
+
+const exp1 = 2 + 2;
+const exp2 = 2 * 2;
+const exp3 = 2 ** 2; //kvadratu
+
+console.log(exp1, exp2, exp3);
+
+const exp4 = 2 ** 0;
+const exp5 = 2 ** 1;
+const exp6 = 2 ** 3;
+const exp7 = 2 ** 4;
+
+console.log(exp4, exp5, exp6, exp7);
+
+const exp9 = 3 ** 3;
+console.log(exp9);
