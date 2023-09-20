82a2ba5efa823837230d63f247a50fd0745a44bd
Laurynas <klumbyslaurynas@gmail.com>
2023-09-20 13:14:27 +0300

    index.js

--- kintamieji/index.js
+++ kintamieji/index.js
@@ -7,3 +7,28 @@ KINTAMIEJI
 - array (kolekcija / sarasas / listas)
 - object (objektas - kompleksiniai informacijai aprasyti)
 */
+
+
+// 1. Kintamieji - kur laikoma informacija
+// 2. Palyginimas / salygos - "jei ...., priesingu atveju ..."
+// 3. Ciklai - kartojimai...
+// 4. Funkcijos - loginiu zingsniu visuma, kuria galima atkartoti, kai reikia
+// 5. Ivykiai - kai vartotojas: paspaudzia mygtuka...
+
+
+/*
+    1. Kintamieji - kur laikoma informacija
+    2. Palyginimas / salygos - "jei ...., priesingu atveju ..."
+    3. Ciklai - kartojimai...
+    4. Funkcijos - loginiu zingsniu visuma, kuria galima atkartoti, kai reikia
+    5. Ivykiai - kai vartotojas: paspaudzia mygtuka...
+*/
+
+
+// Single line comment
+
+/* 
+    Multi
+    line
+    comment
+*/
