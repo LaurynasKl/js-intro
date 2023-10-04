464b43360ea934a21188afcfe3a349ebdc6ca5cd
Laurynas <klumbyslaurynas@gmail.com>
2023-10-04 11:22:12 +0300

    vardas

--- uzd/vardas.js
+++ uzd/vardas.js
@@ -32,7 +32,7 @@ function looksLikeName(text) {
     const neleistiniSimboliai = '0123456789_';
     for( let i = 0 ; i < neleistiniSimboliai.length; i++) {
         const simbolis = neleistiniSimboliai[i];
-        if(text.includes(simbolis)){
+        if(simbolis.includes(neleistiniSimboliai)){
             return `Klaida: ${simbolis} nera leistinas simbolis`;
         }
     }
@@ -51,3 +51,94 @@ console.log(looksLikeName('5Azuolas'), false);
 console.log(looksLikeName('Azuola7s'), false);
 console.log(looksLikeName('Azuolas5'), false);
 console.log(looksLikeName('Eg_le'), false);
+
+console.clear();
+
+function isName(name) {
+    if(typeof name !== 'string'){
+        return 'Klaida: netinkamas duomens tipas, privalo buti "string".';
+    }
+
+    const nameMinLength = 2;
+    if(name.length < nameMinLength) {
+        return `Klaida: per trumpa vardas; minimum ${nameMinLength} simboliai.`
+    }
+
+    const nameMaxLength = 20;
+    if(name.length > nameMaxLength) {
+        return `Klaida: per ilgas vardas: maximum ${nameMaxLength} simboliu.`
+    }
+
+    const pirmaRaide = name[0];
+    if(name[0].toUpperCase() !== name[0] ){
+        return 'Klaida: pirma raide turi buti didzioji'
+    }
+
+    const likusiosRaides = name.slice(1);
+    if(likusiosRaides.toLowerCase() !== likusiosRaides){
+        return 'Klaida: likusios raides turi buti mazosios';
+    }
+
+    // Turime leistinu simboliu sarasa
+    const abc = 'abcdefghijklmnoqprstuvwxyz';
+    const nameLowerCase = name.toLowerCase();
+
+    // einu per vardo raides
+    // pasiimu viena is vardo raidziu
+    // paatikrinu ar ta raide yra sarase
+    // jeigu yra tesiam darba
+    // jei nera - baigiam darba
+
+    for(let i = 0; i < name.length; i++) {
+        const raide = nameLowerCase[i];
+        if(!abc.includes(raide)) {
+            return `Klaida: neleistinas simbolis varde "${raide}".`
+        }
+    }    
+    return name;
+}
+console.log(isName());
+console.log(isName(undefined));
+console.log(isName(null));
+console.log(isName(13));
+console.log(isName(NaN));
+console.log(isName(Infinity));
+console.log(isName(true));
+console.log(isName(false));
+console.log(isName([]));
+console.log(isName({}));
+console.log(isName(isName));
+
+console.log(isName(''));
+console.log(isName('L'));
+console.log(isName('Liiiiiiiiiiiiiiiiiiiii'));
+
+console.log(isName('maryte'));
+console.log(isName('ona'));
+console.log(isName('petras'));
+
+console.log(isName('MarYte'));
+console.log(isName('OnA'));
+console.log(isName('PetrAs'));
+
+console.log(isName('Ma?yte'));
+console.log(isName('On4'));
+console.log(isName('Petr s'));
+console.log(isName('9onas'));
+
+console.log(isName('Li'));
+console.log(isName('Liiiiiiiiiiiiiiiiiii'));
+console.log(isName('Jonas'));
+
+
+const miestas = 'Miestas';
+let errors = '';
+
+if(miestas.length < 10) {
+    errors += 'per trumpas zodis'
+}
+if(miestas.length > 20) {
+    errors += 'per ilgas zodis'
+}
+
+console.log(errors);
